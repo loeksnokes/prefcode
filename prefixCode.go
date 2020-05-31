8672aaa3aee06e1a3f30999b0d995005cd3f0c70
Loeksnokes <cb211@st-andrews.ac.uk>
2020-05-31 20:06:53 +0100

    Basic working prefcode package backed by tests of all core functionality.

--- prefixCode.go
+++ prefixCode.go
@@ -1,23 +1,28 @@
 package prefcode
 
 import (
+	"fmt"
+	"math"
 	"sort"
 	"strconv"
 	"strings"
 	"unicode/utf8"
 )
 
-/*
-PrefCode:
-==========================
-==========================
+// FAILURE is a global constant for a failure.
+const FAILURE = -1
 
-An interface built specifically for the type prefixCode.
+/*
+PrefCode An interface built specifically for the type prefixCode.
 
 prefixCode::
 
 Has:
-alphabet []rune:  The alphabet is ordered by natural rune ordering but listing in slice is arbitrary.
+alphabet []rune:  The alphabet is ordered by natural rune ordering but
+	listing in slice is arbitrary.  The unicode code point 𝛆 which in
+	utf-8 is "0xF0 0x9D 0x9B 0x86" is forbidden for the alphabet
+	(representing the empty string for the empty prefix code)
+
 
 code map[string]int:  The strings are the elements of a finite
 complete prefix code in dictionary order over the alphabet of
@@ -39,19 +44,29 @@ Can:
 TODO: safety checking: expandAt/reduceAt do not currently check if string is legal for alphabet.
 */
 
+// EmptyString will be represented by the string "𝛆"
+const EmptyString = "𝛆"
+
 // PrefCode is interface for struct prefixCode which attempts to represent only
 // complete finite prefix codes over a finite alphabet.
 type PrefCode interface {
 	Alphabet() []rune
 	SetAlphabet([]rune)
-	Equals(p *PrefCode) bool
-	ReduceAt(s string)
-	ExpandAt(s string)
-	//	Join(p1, p2 PrefixCode) *PrefixCode
-	//	Meet(p1, p2 PrefixCode) *PrefixCode
+	Equals(*PrefCode) bool
+	ReduceAt(s string) bool
+	ExpandAt(s string) bool
+	ApplyPerm(perm map[int]int) bool
+	SwapPermAtKeys(a, b string)
+	Permutation() map[int]int
+	Join(PrefCode) PrefCode
+	Meet(PrefCode) PrefCode
 	ExposedCarets() []string
+	LabelAtLeaf(string) int
+	LeafAtLabel(int) string
 	Size() int
 	String() string
+	GetPrefixOf(string) string
+	CodeToSlice() *[]string
 }
 
 type prefixCode struct {
@@ -62,21 +77,163 @@ type prefixCode struct {
 // NewPrefCode returns a prefixCode as a PrefCode.  Magically sets the alphabet to be "01".
 // use NewPrefCodeAlpha to instantiate a code with a different alphabet.
 func NewPrefCode() PrefCode {
-	var prefc prefixCode
-	prefc.alphabet = []rune("01")
-	prefc.code = map[string]int{"0": 0, "1": 1}
+	prefc := NewPrefCodeAlphaString("01")
+
 	return prefc
 }
 
-// NewPrefCodeAlpha returns a prefixCode as a PrefCode and sets alphabet of runes by input string.
-func NewPrefCodeAlpha(alphaStr string) PrefCode {
+// NewPrefCodeAlphaRunes returns a prefixCode as a PrefCode and sets alphabet of runes by slice.
+func NewPrefCodeAlphaRunes(alpha []rune) PrefCode {
 	var prefc prefixCode
-	prefc.alphabet = MakeAlphabet(alphaStr)
-	prefc.code = make(map[string]int)
-	for k, v := range prefc.alphabet {
-		prefc.code[string(v)] = k
+
+	//verify that 𝛆 is not a rune in alpha
+	//TODO handle gracefully with better error handling.
+	for _, v := range alpha {
+		if EmptyString == string(v) {
+			panic("Forbidden character `𝛆` in alphabet.")
+		}
 	}
+
+	if len(alpha) < 1 {
+		panic("Empty Alphabet forbidden.")
+	}
+
+	prefc.alphabet = alpha
+	prefc.code = make(map[string]int, len(alpha))
+	prefc.code[EmptyString] = 0
 	return prefc
+}
+
+// NewPrefCodeAlphaString returns a prefixCode as a PrefCode and sets alphabet of runes by input string.
+func NewPrefCodeAlphaString(alphaStr string) PrefCode {
+	prefc := NewPrefCodeAlphaRunes(MakeAlphabet(alphaStr))
+	return prefc
+}
+
+// DFSToPrefCode takes an alphabet of runes and a properly shaped DFS sequence
+// for alphabet cardinality and creates the corresponding prefixcode with maturnal
+// permutation.
+// TODO: move to prefcode package.
+func DFSToPrefCode(pc PrefCode, DFS string) bool {
+
+	if nil == pc {
+		fmt.Println("DFSToPrefCode called with nil  *PrefCode and DFS: " + DFS)
+		return false
+	}
+	//fmt.Println("DFSToPrefCode with pc: " + (*pc).String() + " and DFS: " + DFS)
+	alpha := pc.Alphabet()
+
+	if !ValidDFSForPrefC(len(alpha), DFS) {
+		fmt.Println("DFSToPrefCode: Failed ValidDFSForPrefC(" + string(int(len(alpha)+'0')) + ", " + DFS + ")")
+		//TODO better error handling.
+		return false
+	}
+	var leaves []string
+
+	// prep working stack of active words (might be extended)
+	var stack []string
+	currentWord := ""
+	stack = append(stack, currentWord)
+	top := 0 //index of top element
+
+	// working alphabet in reverse order
+	alphaSize := len(alpha)
+	revAlpha := make([]rune, alphaSize)
+	for ii := range alpha {
+		revAlpha[ii] = alpha[alphaSize-1-ii]
+	}
+
+	for k, v := range DFS {
+		if `1` == string(v) { //pop TopOStack and push alphaSize new strings on
+			stack = stack[:top] //pop
+
+			for _, letter := range revAlpha {
+				stack = append(stack, currentWord+string(letter))
+			}
+			top = len(stack) - 1
+			currentWord = stack[top]
+			continue
+		}
+		if `0` == string(v) { //top of stack is a leaf.  Move it to leaves.
+			leaves = append(leaves, stack[top])
+			stack = stack[:top]
+			top--
+			if top < 0 {
+				if k == len(DFS)-1 {
+					break
+				}
+				// The tree filled its leaves prematurely:
+				// poorly formatted.  Return Empty prefc
+				return false
+			}
+			currentWord = stack[top]
+		}
+	}
+
+	// cores is the set of words creted by deleting last letter of leaves.  Will
+	// contan the exposed caret roots.
+	cores := make(map[string]bool, len(leaves))
+	for _, v := range leaves {
+		if 0 < len(v) {
+			v = v[:len(v)-1]
+			cores[v] = true
+		}
+	}
+	//fmt.Println("DFSToPrefCode with pc: " + (*pc).String() + " before expansions.")
+
+	for k := range cores {
+		if !pc.ExpandAt(k) {
+			continue
+		}
+		//fmt.Println("DFSToPrefCode with pc: " + (*pc).String() + " after " + k + " expansion.")
+	}
+	//fmt.Println("DFSToPrefCode with pc: " + (*pc).String() + " after expansions.")
+
+	return true
+}
+
+// ValidDFSForPrefC takes an integer (alphabet size) an a puported DFS string
+// and verifies the string is well formatted.
+func ValidDFSForPrefC(alSize int, DFS string) bool {
+	// get number of carets
+	carets := strings.Count(DFS, "1")
+	leaves := strings.Count(DFS, "0")
+
+	if leaves != (((alSize - 1) * carets) + 1) {
+		return false
+		//		panic("Wrong number of leaves in DFS tree "+DFS+" for alphabet size: "+string(`0`+alSize))
+	}
+
+	if !strings.HasPrefix(DFS, "1") {
+		return false
+	}
+	tally := 1
+	countLeaves := 0
+	totalCount := 0
+
+	for _, v := range DFS {
+		totalCount++
+		if "1" == string(v) {
+			tally = tally + alSize - 1
+		}
+		if "0" == string(v) {
+			tally--
+			countLeaves++
+
+		}
+		if 0 == tally && totalCount < len(DFS) {
+			// poorly formed DFS string.
+			return false
+		}
+	}
+
+	if countLeaves != (((alSize - 1) * carets) + 1) {
+		return false
+		//		panic("Wrong number of leaves in DFS tree "+DFS+" for alphabet size: "+string(`0`+alSize))
+	}
+
+	return true
+
 }
 
 //returns a ptr to a copy of the alphabet runes.
@@ -90,6 +247,102 @@ func (p prefixCode) Alphabet() []rune {
 
 func (p prefixCode) Size() int {
 	return len(p.code)
+}
+
+func (p prefixCode) Permutation() (perm map[int]int) {
+	perm = make(map[int]int, len(p.code))
+	keys := make([]string, 0, len(p.code))
+	for k := range p.code {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for ii, k := range keys {
+		perm[ii] = p.code[k]
+	}
+	return
+}
+
+func (p prefixCode) SwapPermAtKeys(a, b string) {
+	valuea, oka := p.code[a]
+	valueb, okb := p.code[b]
+	if oka && okb {
+		p.code[a] = valueb
+		p.code[b] = valuea
+	}
+
+	//todo send some error too if a or b not found.
+	return
+}
+
+// LabelAtLeaf returns the label at the leaf if it exists.
+// If not, returns FAILURE global constant
+func (p prefixCode) LabelAtLeaf(leaf string) (label int) {
+	label, ok := p.code[leaf]
+
+	if !ok {
+		return FAILURE
+	}
+	return
+}
+
+// LeafAtLabel returns the leaf which carries the label, if the
+// label is in bound, or the empty string otherwise.
+func (p prefixCode) LeafAtLabel(label int) (leaf string) {
+	//return empty string if label is out of bounds.
+	//TODO: put in real error handling.
+	if label > (p.Size()-1) || label < 0 {
+		leaf = ""
+		return
+	}
+
+	// find leaf with this label and return it.
+	for k, v := range p.code {
+		if v == label {
+			leaf = k
+			break
+		}
+	}
+	return
+}
+
+// ApplyPerm applies a permutation map to the values of int
+// labels carried by the prefixes
+func (p prefixCode) ApplyPerm(perm map[int]int) bool {
+	if len(p.code) != len(perm) {
+		// TODO: add return for err that bad request was made.
+		return false
+	}
+
+	//assumes (w/o testing) values of p.code are 0 -- k-1
+	//for size k code, and likewise for perm.
+	for k, v := range p.code {
+		p.code[k] = perm[v]
+	}
+	return true
+}
+
+// PermToString converts a map[int]int into a string.
+// Example Output: "[0 5], [1 1], [2 2], [3 3], [4 4], [5 0]"
+func PermToString(permutation map[int]int) (permStr string) {
+
+	// we reexpress the permutation as an ordered slice then print
+	// to avoid map ordering weirdness.
+	sortedPerm := make([]int, len(permutation))
+	for k, v := range permutation {
+		sortedPerm[k] = v
+	}
+
+	for ii, v := range sortedPerm {
+		permStr += "[" + strconv.Itoa(ii) + " " + strconv.Itoa(v) + "]" + ", "
+	}
+
+	trimLen := len(permStr) - 2
+	if trimLen < 0 {
+		trimLen = 0
+	}
+	permStr = strings.TrimSuffix(permStr, ", ")
+	return
 }
 
 func (p prefixCode) String() string {
@@ -123,7 +376,18 @@ func (p prefixCode) Equals(q *PrefCode) bool {
 
 //ReduceAt replaces tree dangling at s with
 //just s and updates values of the PrefixCode.
-func (p prefixCode) ReduceAt(s string) {
+func (p prefixCode) ReduceAt(s string) bool {
+
+	// Handle request to collapse whole PrefCode
+	if "" == s || EmptyString == s {
+		p.code = make(map[string]int, len(p.alphabet))
+		p.code[EmptyString] = 0
+		return true
+	}
+
+	// Now we face a normal request.
+	// we look for s as shallower than some codes.  All such codes are
+	// collapsed to s.  The permutation is re-indexed appropriately.
 	foundCount := 0
 	foundKey := false
 	firstFoundix := len(p.code)
@@ -151,80 +415,110 @@ func (p prefixCode) ReduceAt(s string) {
 			}
 		}
 	}
-	//	fmt.Println("ReduceAt(" + s + ") result: ")
-	//	fmt.Println(p.String())
-
+	if foundCount > 0 {
+		return true
+	}
+	return false
 }
 
 //expandAt adds a dangling tree to the prefix r of t
 //that resides in the PrefixCode, if such exists.  It
 //adds the minimal tree rooted at r so that the result
 //contains t as a member of the code.
-func (p prefixCode) ExpandAt(s string) {
-	//how much do we need to add to expand to s from p.
+func (p prefixCode) ExpandAt(s string) bool {
+
+	// p.code is empty (contains EmptyString) and requested expansion is at root.
+	if (EmptyString == s || "" == s) && 1 == len(p.code) && EmptyString == p.LeafAtLabel(0) {
+		for k, v := range p.alphabet {
+			p.code[string(v)] = k
+		}
+		delete(p.code, EmptyString)
+		return true
+	}
+
+	// code is empty (contains EmptyString) and requested expansion is not at root.
+	// (Implicit from last "If".)
+	// Develop one level of p.code, then pretend we are just starting from the normal case,
+	// but without the EmptyString entry in the code now.
+	if 1 == len(p.code) && EmptyString == p.LeafAtLabel(0) {
+		for k, v := range p.alphabet {
+			p.code[string(v)] = k
+		}
+		delete(p.code, EmptyString)
+		//do not return.  We will now pretend code was not empty and carry on.
+	}
+
+	var labelAtP int
+	prefix := ""
 	var lengthDiff int
-	var prefix string
-	var indexP int
 	var numberNewCodes int
-	var buildSpine []rune
+	buildSpine := []rune(s)
+
+	//general handling
 	var toAppend []string
-	//fmt.Println("ExpandAt(" + s + ") start: ")
-	//fmt.Println(p.String())
 
 	// this is all made more complicated as our string
 	// has runes, not chars, so slices index poorly (by my current reading)
+	// find expandAt location.
 	for k, v := range p.code {
 		if strings.HasPrefix(s, k) { //if s has k as a prefix ...
-			indexP = v
+			labelAtP = v
 			prefix = k
 			lengthDiff = len(s) - len(k)
 			numberNewCodes = lengthDiff*(len(p.alphabet)-1) + len(p.alphabet)
-			buildSpine = []rune(strings.TrimPrefix(s, k))
-			//index counting strings we are creating to add to the PrefCode
-			ii := 0
-			//container for new strings
-			toAppend = make([]string, numberNewCodes)
-			if lengthDiff > 0 {
-				var v rune
-				for jj := 0; jj < len(buildSpine); jj++ {
-					v = buildSpine[jj]
-					for _, r := range p.alphabet {
-						if r != v {
-							// maybe incorrect conversion back and forth for runes
-							// slices are structured over bytes.
-							toAppend[ii] = string(buildSpine[:jj]) + string(r)
-							ii++
-						}
-					}
+			if 0 < lengthDiff {
+				buildSpine = buildSpine[len(k):] // throw away the prefix
+				break
+			}
+			buildSpine = buildSpine[:0] //force buildSpine to be empty
+			break
+		}
+	}
+	if "" == prefix { //code is not empty but no prefix found: expansion location too shallow so do nothing.
+		return false
+	}
+
+	//OK, we found a prefix and built spine for additions.
+	ii := 0
+	//container for new strings
+	toAppend = make([]string, numberNewCodes)
+	if lengthDiff > 0 {
+		var v rune
+		for jj := 0; jj < len(buildSpine); jj++ {
+			v = buildSpine[jj]
+			for _, r := range p.alphabet {
+				if r != v {
+					// maybe incorrect conversion back and forth for runes
+					// slices are structured over bytes.
+					toAppend[ii] = string(buildSpine[:jj]) + string(r)
+					ii++
 				}
 			}
-			// full alphabet expansion one rune beyond s
-			for _, r := range p.alphabet {
-				toAppend[ii] = string(buildSpine[:]) + string(r)
-				ii++
-			}
 		}
+	}
+	// full alphabet expansion one rune beyond s
+	for _, r := range p.alphabet {
+		toAppend[ii] = string(buildSpine[:]) + string(r)
+		ii++
 	}
 
 	if nil != toAppend {
 		sort.Strings(toAppend)
-
 		// delete k from p.code.
 		// then reindex the later keys by adding numberNewCodes-1
 		// (we are adding numberNewCodes new strings but deleted one)
 		// then insert the new codes to the prefixCode
 		delete(p.code, prefix)
 		for lateKey, v := range p.code {
-			if v > indexP {
+			if v > labelAtP {
 				p.code[lateKey] = v + numberNewCodes - 1
 			}
 		}
 		for jj, v := range toAppend {
-			p.code[prefix+v] = indexP + jj
+			p.code[prefix+v] = labelAtP + jj
 		}
 	}
-	//fmt.Println("ExpandAt(" + s + ") result: ")
-	//fmt.Println(p.String())
+	return true
 }
 
 func (p prefixCode) ExposedCarets() (caretRoots []string) {
@@ -256,15 +550,92 @@ func (p prefixCode) ExposedCarets() (caretRoots []string) {
 	return
 }
 
-/*
-func (p PrefixCode) Join(p1, p2 PrefixCode) *PrefixCode{
-
+func (p prefixCode) GetPrefixOf(s string) string {
+	for k := range p.code {
+		if strings.HasPrefix(s, k) {
+			return k
+		}
+	}
+	return ""
 }
 
-func (p PrefixCode) Meet(p1, p2 PrefixCode) *PrefixCode{
+// Join finds smallest prefix code so that each leaf is deeper/equal
+// to leaves of both prefix codes and returns a pointer to this constructed code.
+// TODO: needs testing coverage
+func (p prefixCode) Join(q PrefCode) PrefCode {
+	jpc := NewPrefCodeAlphaRunes(p.alphabet)
 
+	expansionsP := p.ExposedCarets()
+
+	for _, v := range expansionsP {
+		jpc.ExpandAt(v)
+	}
+
+	expansionsQ := q.ExposedCarets()
+
+	for _, v := range expansionsQ {
+		jpc.ExpandAt(v)
+	}
+	return jpc
 }
-*/
+
+// Iterates from left-right through the prefx codes, choosing the shallower
+// element of any comparable pair too build a new prefix code.  Replaces the first with this one.
+func (p prefixCode) Meet(q PrefCode) PrefCode {
+	jpc := NewPrefCodeAlphaRunes(p.alphabet)
+
+	expansionsP := p.ExposedCarets()
+	expansionsQ := q.ExposedCarets()
+
+	var vRunes []rune
+	var wRunes []rune
+
+	allCommonExpansions := make(map[string]bool)
+	var maxLen int
+	var commonWord string
+
+	// for each exposed caret of p and q
+	// intersect words to find longest common prefix
+	// the list of these can be used to expand out
+	// the common tree.
+	// It is OK to have several words that are extensions
+	// of each other here since the expansion code will do nothing
+	// when expanding shallower than the tree (it is unclear
+	// if sorting to get rid of these is faster than just letting
+	// a few duplicate expansions run)
+
+	for _, v := range expansionsP {
+		for _, w := range expansionsQ {
+			maxLen = int(math.Min(float64(len(v)), float64(len(w))))
+			vRunes = []rune(v)
+			wRunes = []rune(w)
+			for ii := 0; ii < maxLen; ii++ {
+				if vRunes[ii] == wRunes[ii] {
+					commonWord = commonWord + string(vRunes[ii])
+					continue
+				}
+				break
+			}
+			if len(commonWord) > 0 {
+				allCommonExpansions[commonWord] = true
+				commonWord = ""
+			}
+		}
+	}
+	for k := range allCommonExpansions {
+		jpc.ExpandAt(k)
+	}
+	return jpc
+}
+
+// CodeToSlice returns a * to slice consisting of the codestrings of p
+func (p prefixCode) CodeToSlice() *[]string {
+	codes := make([]string, len(p.code))
+	for k := range p.code {
+		codes = append(codes, k)
+	}
+	return &codes
+}
 
 // Helper functions many just found and mildly edited from standard websites.
 
--- prefixCode_test.go
+++ prefixCode_test.go
@@ -11,16 +11,17 @@ import (
     2) expand at a location.
     3) reduce at a location.
     4) list exposed carets.
-	5) print itself.
+	5) print itself as prefcode.
+	6) swap labels on two leaves (SwapPermAtKeys)
+	7) change permutation of labels (ApplyPerm)
+	8) print perm to string (natural order leaves as ints are index for labels.)
+	9) Meet of two prefix codes
+	10)join of two prefix codes
 
 	TODO (23-05-2020):
-	1) Meet and Join.
-	2) say which part of the code is a prefix of a long enough entry.
-	3) list integer values across an exposed caret in alphabet order of children.
-
-	Currently, a prefix code must have at least one child for each letter of
-	alphabet: the empty string is not a prefix code, and this should probably
-	be changed.
+	1) Better tests around ExpandAt and DFS string creation
+	2) Testing output as DFS string.
+	3) Test ability to say which part of the code is a prefix of a long enough entry.
 */
 func Test(t *testing.T) {
 
@@ -32,22 +33,15 @@ func Test(t *testing.T) {
 	}
 
 	// String gives correctly formatted output
-	t.Run("ExpandAt deeper than prefcode.", func(t *testing.T) {
-		baseCodeToExpand := NewPrefCodeAlpha("日本語")
-		baseCodeToExpand.ExpandAt("本")
-		assertCorrectMessage(t, baseCodeToExpand.String(), "[日 0], [本日 1], [本本 2], [本語 3], [語 4]")
-	})
-
-	// Size gives correclty formatted output
-	t.Run("ExpandAt deeper than prefcode.", func(t *testing.T) {
-		baseCodeToExpand := NewPrefCodeAlpha("日本語")
+	t.Run("ExpandAt deeper than prefcode unicodealphastring.", func(t *testing.T) {
+		baseCodeToExpand := NewPrefCodeAlphaString("日本語")
 		baseCodeToExpand.ExpandAt("本")
 		assertCorrectMessage(t, strconv.Itoa(baseCodeToExpand.Size()), "5")
 	})
 
 	// ExpandAt("here") for "here" having an element of code as a proper prefix
 	// (expands to minimal prefix code with dangling caret at "here").
-	t.Run("ExpandAt deeper than prefcode.", func(t *testing.T) {
+	t.Run("ExpandAt deeper than prefcode no init alphabet.", func(t *testing.T) {
 		baseCodeToExpand := NewPrefCode()
 		baseCodeToExpand.ExpandAt("1001")
 		assertCorrectMessage(t, baseCodeToExpand.String(), "[0 0], [1000 1], [10010 2], [10011 3], [101 4], [11 5]")
@@ -55,7 +49,8 @@ func Test(t *testing.T) {
 
 	// ExpandAt("here") for "here" properly in the body of tree.
 	t.Run("ExpandAt shallower than prefcode.", func(t *testing.T) {
-		baseCodeToExpand := NewPrefCode()
+		myRunes := StringToRuneSlice("01")
+		baseCodeToExpand := NewPrefCodeAlphaRunes(myRunes)
 		baseCodeToExpand.ExpandAt("1001")
 		baseCodeToExpand.ExpandAt("1")
 		assertCorrectMessage(t, baseCodeToExpand.String(), "[0 0], [1000 1], [10010 2], [10011 3], [101 4], [11 5]")
@@ -90,5 +85,77 @@ func Test(t *testing.T) {
 		want := "1001 11"
 		assertCorrectMessage(t, got, want)
 	})
+
+	// Join Test.
+	t.Run("Join a right vine tree to a left vine tree.", func(t *testing.T) {
+		myRunes := StringToRuneSlice("01")
+		pcFirst := NewPrefCodeAlphaRunes(myRunes)
+		pcFirst.ExpandAt("0001")
+		pcSecond := NewPrefCodeAlphaRunes(myRunes)
+		pcSecond.ExpandAt("1101")
+		pcJoin := pcFirst.Join(pcSecond)
+		got := pcJoin.String()
+		want := "[0000 0], [00010 1], [00011 2], [001 3], [01 4], [10 5], [1100 6], [11010 7], [11011 8], [111 9]"
+		assertCorrectMessage(t, got, want)
+	})
+
+	// Meet Test
+	t.Run("Meet a right vine tree to anothertree.", func(t *testing.T) {
+		myRunes := StringToRuneSlice("01")
+		pcFirst := NewPrefCodeAlphaRunes(myRunes)
+		pcFirst.ExpandAt("1101")
+		pcSecond := NewPrefCodeAlphaRunes(myRunes)
+		pcSecond.ExpandAt("1111")
+		pcMeet := pcFirst.Meet(pcSecond)
+		got := pcMeet.String()
+		want := "[0 0], [10 1], [110 2], [111 3]"
+		assertCorrectMessage(t, got, want)
+	})
+
+	// SwapPermAtKeys swaps label values at two prefixcode keys.
+	t.Run("Checking SwapPermAtKeys.",
+		func(t *testing.T) {
+			baseCode := NewPrefCode()
+			baseCode.ExpandAt("1001")
+
+			baseCode.SwapPermAtKeys("0", "11")
+
+			got := PermToString(baseCode.Permutation())
+			want := "[0 5], [1 1], [2 2], [3 3], [4 4], [5 0]"
+
+			assertCorrectMessage(t, got, want)
+		})
+
+	// ApplyPerm applies a given permutation to the integer key labels.
+	t.Run("Checking ApplyPerm.",
+		func(t *testing.T) {
+			baseCode := NewPrefCode()
+			baseCode.ExpandAt("1001")
+			baseCode.SwapPermAtKeys("0", "11")
+			baseCode.ApplyPerm(map[int]int{0: 3, 1: 5, 2: 1, 3: 0, 4: 2, 5: 4})
+
+			got := PermToString(baseCode.Permutation())
+			want := "[0 4], [1 5], [2 1], [3 0], [4 2], [5 3]"
+			assertCorrectMessage(t, got, want)
+		})
+
+	// Checks whether we can use := to copy prefix codes.
+	t.Run("Checking assignment.",
+		func(t *testing.T) {
+			baseCode := NewPrefCode()
+			baseCode.ExpandAt("1001")
+			baseCode.SwapPermAtKeys("0", "11")
+			baseCode.ApplyPerm(map[int]int{0: 3, 1: 5, 2: 1, 3: 0, 4: 2, 5: 4})
+
+			var targetCode PrefCode
+
+			targetCode = baseCode
+
+			got := targetCode.String()
+			//fmt.Println("got:  " + got)
+			want := baseCode.String()
+			//fmt.Println("want: " + want)
+			assertCorrectMessage(t, got, want)
+		})
 
 }
